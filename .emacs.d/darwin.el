dd278bd768f5185f0621dfbc1a281fdd213f7982
Renaud Tircher <rtircher@thoughtworks.com>
2012-02-25 23:49:54 -0800

    Updating default font and frame size

--- .emacs.d/darwin.el
+++ .emacs.d/darwin.el
@@ -1,3 +1,7 @@
 (setq ga-plt-font "inconsolata")
-(setq ga-plt-font-size 120)
+(setq ga-plt-font-size 140)
 (setenv "PATH" (concat "/usr/local/bin" path-separator (getenv "PATH")))
+
+;; Set default emacs frame size
+(add-to-list 'default-frame-alist '(height . 70))
+(add-to-list 'default-frame-alist '(width . 200))
